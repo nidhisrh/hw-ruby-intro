396085a96434cea874db65354caebe0d3529a549
EC2 Default User <ec2-user@ip-172-31-6-140.us-east-2.compute.internal>
2018-09-11 10:14:47 +0000

    all sets
    exit
    
    git add --all

--- lib/ruby_intro.rb
+++ lib/ruby_intro.rb
@@ -3,33 +3,101 @@
 # Part 1
 
 def sum arr
-  # nidhi sirohi
+  Integer s = 0
+  arr.each do |i|
+    s = s + i
+  end
+  return s
 end
 
 def max_2_sum arr
-  # YOUR CODE HERE
+  Integer s = 0;
+
+  if arr.length == 1
+    s =  arr[0]
+  elsif arr.length > 0
+    a = -(2**(0.size * 8 -2))
+    b = a
+    arr.each do |i|
+      if i > a
+        b = a
+        a = i
+      else
+        if i > b
+          b = i
+        end
+      end
+    end
+    s = a+b
+  end
+  return s
 end
 
 def sum_to_n? arr, n
-  # YOUR CODE HERE
+  res = false
+  if arr.length > 1
+    s = 0
+    e = arr.length - 1
+    arr = arr.sort
+    while s < e do
+      if arr[s] + arr[e] > n
+        e = e-1
+      elsif arr[s] + arr[e] < n
+        s = s+1
+      else
+        res = true
+        break
+      end
+    end
+  end
+  return res
 end
 
 # Part 2
 
 def hello(name)
-  # YOUR CODE HERE
+  return "Hello, "+name
 end
 
 def starts_with_consonant? s
-  # YOUR CODE HERE
+  return s.match(/\A(?=[^aeiouAEIOU])(?=[a-zA-Z])/i)
 end
 
 def binary_multiple_of_4? s
-  # YOUR CODE HERE
+  return s.match(/\A(([10]*00)|(0{1}))\Z/)
 end
 
 # Part 3
 
 class BookInStock
-# YOUR CODE HERE
+  def initialize(isbn,price)
+    if(isbn==''||price <= 0)
+      raise ArgumentError, 'Invalid arguements'
+    end
+    @isbn = isbn
+    @price = price
+  end
+  def price
+    @price
+  end
+  def price=  (p)
+    @price = p
+  end
+  def isbn
+    @isbn
+  end
+  def isbn=  (i)
+    @isbn = i
+  end
+  def price_as_string
+    p = (@price*100)%100
+    s =  p.to_i
+    if(s==0)
+      s="00"
+    else
+      s=s.to_s
+    end
+    return "$"+@price.to_i.to_s+"."+s
+  end
 end
+
